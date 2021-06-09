04a0a789d37690e25eb62622b393e563eca894ff
Andersson Brito <andersson.brito@wattei.com.br>
2021-06-09 13:30:24 -0300

    conexao banco

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
+const mongoose = require('mongoose');
+
 require('dotenv').config({path:'variables.env'})
+
+//Conexão MongoDB
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('errro', (errro) => {
+    console.log("ERRO:" + error.mensage);
+});
 
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () =>{
